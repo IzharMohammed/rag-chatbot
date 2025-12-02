a4d18ae466e3164818ba3bcd31a02ed3fe639b1a
IzharMohammed <izharmohammed21@gmail.com>
2025-12-02 19:21:54 +0530

    add flow of project

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,21 @@ import { Message, UploadedFile, TokenUsage, DetailedTokenUsage } from "@/types";
 import FileUpload from "@/components/kokonutui/file-upload";
 import { FileText, Sparkles, Upload, X } from "lucide-react";
 
+/**
+ * Implementation plan
+ * Stage 1: Indexting
+ * 1. Load the document - pdf, text - completed
+ * 2. Chunk the document - completed
+ * 3. Generate vector embeddings - completed
+
+ *
+ * Stage 2: Using the chatbot
+ * 1. Setup LLM 
+ * 2. Add retrieval step
+ * 3. Pass input + relevant information to LLM
+ * 4. Congratulations
+ */
+
 export default function Home() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[]>([]);
